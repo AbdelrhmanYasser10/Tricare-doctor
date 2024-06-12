05bb06d480b29cac2e34d2bf3d920a36f42668e6
Abdelrhman Yasser <abdelrhmanyasser123@gmail.com>
2024-06-12 14:19:43 +0300

    phase1 finished

--- lib/core/component/DropDownButtonField/drop_down_button_filed.dart
+++ lib/core/component/DropDownButtonField/drop_down_button_filed.dart
@@ -29,7 +29,6 @@ class BuildDropDownButtonField extends StatelessWidget {
   Widget build(BuildContext context) {
     return DropdownButtonFormField2(
       autovalidateMode: AutovalidateMode.onUserInteraction,
-
       decoration: InputDecoration(
         isDense: true,
         contentPadding: EdgeInsets.zero,
@@ -38,7 +37,7 @@ class BuildDropDownButtonField extends StatelessWidget {
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide(color: AppColor.primaryColor, width: 2),
+          borderSide: const BorderSide(color: AppColor.primaryColor, width: 2),
         ),
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
@@ -49,21 +48,28 @@ class BuildDropDownButtonField extends StatelessWidget {
       hint: Text(
         hint,
       ),
-      icon: const Icon(
-        Icons.arrow_drop_down,
-        color: Colors.grey,
-        textDirection: TextDirection.ltr,
+      iconStyleData: const IconStyleData(
+        icon: Icon(
+          Icons.arrow_drop_down,
+          color: Colors.grey,
+          textDirection: TextDirection.ltr,
+        ),
+        iconSize: 30,
       ),
-      iconSize: 30,
-      buttonHeight: height * 0.08,
-      buttonPadding: const EdgeInsets.only(left: 20, right: 10),
-      dropdownDecoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
+      dropdownStyleData: DropdownStyleData(
+        maxHeight: height * 0.75,
+        scrollbarTheme: ScrollbarThemeData(
+          thickness: MaterialStateProperty.all(10),
+          radius: const Radius.circular(15),
+        ),
+        elevation: 2,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
-      dropdownMaxHeight: height * 0.75,
-      scrollbarThickness: 10,
-      scrollbarAlwaysShow: true,
-      scrollbarRadius: Radius.circular(15),
+      buttonStyleData: ButtonStyleData(
+          height: height * 0.08,
+          padding: const EdgeInsets.only(left: 20, right: 10)),
       items: item
           .map((item) => DropdownMenuItem<String>(
                 value: item,
@@ -73,7 +79,6 @@ class BuildDropDownButtonField extends StatelessWidget {
               ))
           .toList(),
       validator: valid,
-      dropdownElevation: 2,
       onChanged: onChanged,
       onSaved: (value) {},
     );
